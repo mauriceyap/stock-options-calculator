256dd24ea564a21aab789412594213f6f565dc33
Maurice Yap <mauriceyap@hotmail.co.uk>
2024-08-06 21:02:24 +0100

    Add support for MUI X date pickers

--- src/app/AppContextProvider.tsx
+++ src/app/AppContextProvider.tsx
@@ -1,12 +1,27 @@
+import "dayjs/locale/en-gb";
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { ReactNode, useMemo } from "react";
 
 import { ColourModeProvider } from "../contexts/colourMode/ColourModeProvider";
 
-const providers = [ColourModeProvider];
-
-interface AppContextProviderProps {
+interface ProviderProps {
   children: ReactNode;
 }
+
+const providers = [
+  ColourModeProvider,
+  ({ children }: ProviderProps) => (
+    <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale="en-gb">
+      {children}
+    </LocalizationProvider>
+  ),
+];
+
+export interface AppContextProviderProps {
+  children: ReactNode;
+}
+
 export const AppContextProvider = ({ children }: AppContextProviderProps) => {
   const Provider = useMemo(
     () =>
@@ -19,7 +34,7 @@ export const AppContextProvider = ({ children }: AppContextProviderProps) => {
           );
           return C;
         },
-        ({ children }: AppContextProviderProps) => <>{children}</>
+        ({ children }: AppContextProviderProps) => children
       ),
     []
   );
