fc72b87c19e26e2496fd6d9bef054c9af862e018
Maurice Yap <mauriceyap@hotmail.co.uk>
2024-08-19 20:00:37 +0100

    add compact format to GBP formatter

--- src/common/formatGBP.ts
+++ src/common/formatGBP.ts
@@ -3,4 +3,11 @@ const gbpFormatter = new Intl.NumberFormat("en-GB", {
   currency: "GBP",
 });
 
-export const formatGBP = (amount: number) => gbpFormatter.format(amount);
+const gbpCompactFormatter = new Intl.NumberFormat("en-GB", {
+  style: "currency",
+  currency: "GBP",
+  notation: "compact",
+});
+
+export const formatGBP = (amount: number, compact = false) =>
+  (compact ? gbpCompactFormatter : gbpFormatter).format(amount);
