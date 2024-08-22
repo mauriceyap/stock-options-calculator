11c4328611daade9d078d9270604a6993d2fd81f
Maurice Yap <mauriceyap@hotmail.co.uk>
2024-08-22 18:16:10 +0100

    small visual tweaks to MUI theme to imporve compactness

--- src/themes.ts
+++ src/themes.ts
@@ -1,23 +1,66 @@
-import { createTheme } from "@mui/material";
-import type { TypographyOptions } from "@mui/material/styles/createTypography";
+import { ThemeOptions, createTheme } from "@mui/material/styles";
 
-const typography: TypographyOptions = {
-  button: {
-    textTransform: "none",
+const baseTheme: ThemeOptions = {
+  components: {
+    MuiFormControl: {
+      defaultProps: {
+        size: "small",
+      },
+    },
+    MuiTextField: {
+      defaultProps: {
+        variant: "outlined",
+        size: "small",
+      },
+    },
   },
-  fontFamily: "'Rubik Variable', sans-serif",
+  typography: {
+    button: {
+      textTransform: "none",
+    },
+    fontFamily: ["'Rubik Variable'", "sans-serif"].join(", "),
+    fontSize: 13,
+    h1: {
+      fontSize: "2.4rem",
+      fontWeight: 500,
+    },
+    h2: {
+      fontSize: "2rem",
+      fontWeight: 500,
+    },
+    h3: {
+      fontSize: "1.8rem",
+      fontWeight: 500,
+    },
+    h4: {
+      fontSize: "1.6rem",
+      fontWeight: 500,
+    },
+    h5: {
+      fontSize: "1.4rem",
+      fontWeight: 500,
+    },
+    h6: {
+      fontSize: "1.2rem",
+      fontWeight: 500,
+    },
+  },
+  spacing: 6,
+  shape: {
+    borderRadius: 2,
+  },
 };
 
 export const lightTheme = createTheme({
+  ...baseTheme,
   palette: {
     mode: "light",
   },
-  typography,
 });
 
 export const darkTheme = createTheme({
+  ...baseTheme,
   palette: {
     mode: "dark",
   },
-  typography,
 });
